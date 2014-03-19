b1f5e72b5cc73caa7208235441b8906ee12974f4
Jameson Trinker <jameson.trinker@gmail.com>
2014-03-18 19:44:30 -0500

    have logic working in view, need to refator

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,10 @@
 class GamesController < ApplicationController
+
   def index
-  end
+  	@games = Game.all
+  	# if Time.now.strftime("%_m/%d")[1..-1] == @games[0].date.strftime("%_m/%d")[1..-1]
+  	# 	@todays_game = @games.shift
+   #  end
+  end	
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@ Game.destroy_all
 
 # Game Data
 Game.create(id: 1, date: "3/18", time: "7PM", opponent: "Chicago White Sox", location: "Birmingham", event: "Thirsty Thursdays")
+Game.create(id: 2, date: "4/03", time: "7PM", opponent: "Montgomery", location: "Montgomery, AL", event: nil)
