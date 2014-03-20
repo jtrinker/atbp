6e073518d8921da0c1a9e1fa3ac9b0e4dc0f9545
Jameson Trinker <jameson.trinker@gmail.com>
2014-03-20 16:01:12 -0500

    update seeds, input live data, display countdown to first game info

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@ class GamesController < ApplicationController
 
   def index
   	@games = Game.all
+  	@first_game = Game.first
   	@next_games = Game.where(["date > ?", Time.now]).all
   	@schedule = @next_games[0..4]
   	@phrase = Game.pull_phrase
