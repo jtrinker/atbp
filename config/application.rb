bd116915409fdfafda716612018f76081368bf26
Jameson Trinker <jameson.trinker@gmail.com>
2014-03-21 12:40:48 -0500

    fixed asset issue, now finding fonts

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Atbp
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    # PUll in fonts folder, super headache -- don't forget
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
 
