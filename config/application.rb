57d7ce4ca6912599b3a26ea4ab7c7851256eba85
Jameson Trinker <jameson.trinker@gmail.com>
2014-03-21 13:30:30 -0500

    fix fonts

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Atbp
   class Application < Rails::Application
-    # PUll in fonts folder, super headache -- don't forget
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
   end
 end
 
