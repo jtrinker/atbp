84c22e8690e3146e2545dac4cc385804f6413081
Jameson Trinker <jameson.trinker@gmail.com>
2014-03-23 21:53:13 -0500

    fix dates

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
 
   def index
   	@games = Game.all
-  	@first_game = Game.first
+  	@first_game = Game.find(7)
   	@next_games = Game.where(["date > ?", Time.now]).all
   	@schedule = @next_games[0..4]
   	@phrase = Game.pull_phrase
