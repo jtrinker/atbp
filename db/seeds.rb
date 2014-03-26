5e5586d79326b748992a3d90a7bcbe806e676e0c
Jameson Trinker <jameson.trinker@gmail.com>
2014-03-26 13:58:11 -0500

    edit countdown_phrase list

--- db/seeds.rb
+++ db/seeds.rb
@@ -87,7 +87,14 @@ CountdownPhrase.destroy_all
 
 CountdownPhrase.create(id: 1, line: "What’s on netflix?")
 CountdownPhrase.create(id: 2, line: "time to go muddin")
-CountdownPhrase.create(id: 3, line: "is carrigans?")
 CountdownPhrase.create(id: 4, line: "time to de-pants")
-CountdownPhrase.create(id: 5, line: "lady liberty weeps")
+CountdownPhrase.create(id: 5, line: "drinkin' at good people")
+CountdownPhrase.create(id: 6, line: "googling myself")
+CountdownPhrase.create(id: 7, line: "late brunch at centrale")
+CountdownPhrase.create(id: 8, line: "looking for marty swant")
+CountdownPhrase.create(id: 9, line: "slice of pie at POPs")
+CountdownPhrase.create(id: 10, line: "ridin' ms. ancy")
+CountdownPhrase.create(id: 11, line: "beers at trimtab")
+
+
 
