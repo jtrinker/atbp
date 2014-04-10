e41237d8978693e8a225e8613a47312cfbe51989
Jameson Trinker <jameson.trinker@gmail.com>
2014-04-09 22:21:25 -0500

    remove first game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
 
   def index
   	@games = Game.all
-  	@first_game = Game.find(7)
+  	# @first_game = Game.find(7)
 
   	@next_games = Game.where(["date > ?", Time.now - 1.day]).all
   	@next_home_games = @next_games.select { |game| game.away == false }
