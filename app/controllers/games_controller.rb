b001977343128294027721a18d05050d4e82e8a3
Jameson Trinker <jameson.trinker@gmail.com>
2014-04-11 21:05:35 -0500

    complete test page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,6 +13,7 @@ class GamesController < ApplicationController
   end
 
   def test
+  	@games = Game.all
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Atbp::Application.routes.draw do
   root "games#index"
   resources :games, only: [:index]
 
-  match "/test" => "games_controller#test"
+  match "/test" => "games#test", via: [:get]
 
 end
