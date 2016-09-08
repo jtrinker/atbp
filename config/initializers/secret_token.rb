e8acf066fdcb4357625cf6c27d8e93de42e9bf87
Jameson Trinker <jameson.trinker@gmail.com>
2016-09-07 20:39:19 -0400

    secret key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,3 @@
 # Atbp::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
+Atbp::Application.config.secret_key_base = '8d0cc43ca330a010290a7d7599406cb7d9d9d312293c190ffd891f73f1b5e1602b8d2cb1b2a1a7e79831314be92d773ddb7134d167d454c9c88b11dfca36d3e5'
 
-Atbp::Application.config.secret_key_base = if Rails.env.development? or Rails.env.test?
-  ('x' * 30) # meets minimum requirement of 30 chars long
-else
-  ENV['SECRET_KEY_BASE']
-end
